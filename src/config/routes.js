5c93097e335ce3a66c0c40ecc3fe8c3022822c89
Phuong <phuongphuong262204@gmail.com>
2024-06-24 08:02:04 +0700

    add assets

--- src/config/routes.js
+++ src/config/routes.js
@@ -1,9 +1,6 @@
 const routes = {
     // Home routes
     home: "/",
-    // AboutUs and ContactUs routes
-    aboutus: "/about",
-    contactus: "/contact",
     error: "/error",
 };
 export default routes;
